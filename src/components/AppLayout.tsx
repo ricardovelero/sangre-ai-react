2436a26b7dea709a8bcf9e5fb06ccb8638a43fe6
Ricardo Rodriguez <ricardovelero@gmail.com>
2025-03-11 17:00:35 +0100

    Effect to initializeAuth

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,7 +1,14 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
+import { useAuthStore } from "@/store/authStore";
+import { useEffect } from "react";
 
 export default function AppLayout() {
+  const initializeAuth = useAuthStore((state) => state.initializeAuth);
+
+  useEffect(() => {
+    initializeAuth();
+  }, [initializeAuth]);
   return (
     <div className='mx-auto sm:px-6 lg:px-24'>
       <NavBar />
