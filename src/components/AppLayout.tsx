043018c7a32b94a9e8f74ebfcf93dad641f09b41
Ricardo Rodriguez <ricardovelero@gmail.com>
2025-03-11 14:35:44 +0100

    Add logic

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,3 +1,11 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./NavBar";
+
 export default function AppLayout() {
-  return <div>AppLayout</div>;
+  return (
+    <div className='mx-auto sm:px-6 lg:px-24'>
+      <NavBar />
+      <Outlet />
+    </div>
+  );
 }
