4b5a56207aa62ff557c19fc0ce6544135d99369b
Ricardo Rodriguez <ricardovelero@gmail.com>
2025-03-11 14:36:05 +0100

    Fix name

--- src/components/PublicLayout.tsx
+++ src/components/PublicLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 
-export default function Root() {
+export default function PublicLayout() {
   return (
     <div className='mx-auto sm:px-6 lg:px-24'>
       <NavBar />
