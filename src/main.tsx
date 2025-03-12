e39fc0d8bde4f32f51d47baad530a813d4cbdbcb
Ricardo Rodriguez <ricardovelero@gmail.com>
2025-03-12 11:28:22 +0100

    Install Shadcn ui, remove the other

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import "./index.css";
+import { Toaster } from "./components/ui/sonner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Toaster />
   </React.StrictMode>
 );
