1591058f8d5a83af16b8afbfa33dd1e9d67f16be
Ricardo Rodriguez <ricardovelero@gmail.com>
2025-03-18 16:37:38 +0100

    Fix hero height

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           className='relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
         />
       </div>
-      <div className='mx-auto max-w-2xl py-32 sm:py-48 lg:py-56'>
+      <div className='mx-auto max-w-2xl h-screen py-32 sm:py-48 lg:py-56'>
         <div className='text-center'>
           <h1 className='text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl'>
             Descifra tu analítica con IA
