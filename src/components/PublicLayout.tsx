cd59b2bacdc0c1299b976756a16dc9321f1cfa70
Ricardo Rodriguez <ricardovelero@gmail.com>
2025-04-09 21:25:44 +0200

    Fix min-h footer

--- src/components/PublicLayout.tsx
+++ src/components/PublicLayout.tsx
@@ -6,17 +6,21 @@ import PageTransition from "./PageTransition";
 
 export default function PublicLayout() {
   return (
-    <div className='mx-auto max-w-7xl sm:px-6 lg:px-8'>
+    <div className='min-h-screen flex flex-col'>
       <div className='hidden sm:block'>
         <Navbar />
       </div>
       <div className='sm:hidden'>
         <MobileNavBar />
       </div>
-      <PageTransition>
-        <Outlet />
-      </PageTransition>
-      <Footer />
+      <main className='flex-1 mx-auto max-w-7xl sm:px-6 lg:px-8'>
+        <PageTransition>
+          <Outlet />
+        </PageTransition>
+      </main>
+      <div className='mx-auto max-w-7xl sm:px-6 lg:px-8'>
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/components/PublicNavbar.tsx
+++ src/components/PublicNavbar.tsx
@@ -13,7 +13,7 @@ import { navItems } from "@/lib/navItems";
 export default function NavBar() {
   return (
     <header>
-      <div className='flex items-center justify-between'>
+      <div className='flex items-center justify-between sm:px-6 lg:px-8'>
         <div className='flex items-center justify-evenly'>
           <NavigationMenu className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8'>
             <Logo />
